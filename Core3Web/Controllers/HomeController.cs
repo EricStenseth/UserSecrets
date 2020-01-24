98c38380a8c0177e32ec6233ca4f0d87e952b01a
Eric Stenseth <eric.stenseth@novacare.no>
2020-01-24 15:13:45 +0100

    Added secrets to core and framework

---
+++ Core3Web/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Core3Web.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Core3Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            // get secret value, should be different from appsettings
+            // right click project -> Manage User Secrets
+            /* secrets.json should look like
+             *{
+             *  "MySecret": "very secret"
+             *}
+             */
+            var secret =_configuration["MySecret"];
+            if(secret == "not secret")
+                throw new Exception("Could not find correct secret value");
+
+            return View("Index", secret);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FrameworkWeb/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace FrameworkWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Secret = ConfigurationManager.AppSettings["MySecret"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
